030886aa72a679c839433b67f5db55c05f8aa37d
lvreess <szczurek135@hotmail.com>
2022-09-13 11:02:31 +0200

    Komentarz

---
+++ lekcja 1 wprowadzenie/STR.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+
+
+
+
+<meta charset="utf-8">
+<title>powtórzenie</title>
+
+
+
+
+</head>
+<body>
+<center><h1>HELLO WORLD</h1></center>
+
+<?php
+            $Mycounter=1;
+
+			echo "<p>Hello world!</p>";
+			echo "My counter $Mycounter <br>";
+			echo "<p>Hello friend!</p>";
+			 ?>
+
+</body>
+</html>
